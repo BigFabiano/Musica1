820920bb8c254a1403ab715ccd9f3070c775b723
FabianoEtec <nbscbs@gmail.com>
2021-08-15 14:31:08 -0300

    musica finalizada

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Musica1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("|       Curta Canção         |");
+            Console.WriteLine("------------------------------");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Tudo que se faz só faz sentido");
+            Console.WriteLine("Se vivermos em paz com o mundo");
+            Console.WriteLine("E termos um ao outro como amigo");
+            Console.ResetColor();
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Como numa estória de amor");
+            Console.WriteLine("Que se passou na primavera");
+            Console.WriteLine("Que encontrou a união");
+            Console.WriteLine("");
+            Console.WriteLine("Tenha coragem, o sorriso é a porta da razão");
+            Console.WriteLine("O amor vai durar três eternidades");
+            Console.WriteLine("Roma vai vira no seu pensamento");
+            Console.ResetColor();
+
+        }
+    }
+}
